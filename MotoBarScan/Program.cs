30a73f243c73f1911106e6b959d4f1300f015282
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2015-03-10 21:45:26 +0100

    Initial commit.

---
+++ MotoBarScan/Program.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) 2015, Łukasz Walukiewicz <lukasz@walukiewicz.eu>
+// Licensed under the MIT License <http://lukasz.walukiewicz.eu/p/MIT>
+// Part of the MotoBarScan project <http://lukasz.walukiewicz.eu/p/MotoBarScan>
+
+using System;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using CoreScanner;
+
+namespace MotoBarScan
+{
+    class Program
+    {
+        static CCoreScanner cCoreScannerClass;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                cCoreScannerClass = new CCoreScannerClass();
+
+                short[] scannerTypes = new short[] { 1 };
+                int status;
+
+                cCoreScannerClass.Open(0, scannerTypes, (short)scannerTypes.Length, out status);
+
+                if (status != 0)
+                {
+                    Environment.Exit(1);
+                }
+
+                cCoreScannerClass.BarcodeEvent += cCoreScannerClass_BarcodeEvent;
+
+                string outXML;
+                string inXML = "<inArgs>"
+                    + "<cmdArgs>"
+                    + "<arg-int>1</arg-int>"
+                    + "<arg-int>1</arg-int>"
+                    + "</cmdArgs>"
+                    + "</inArgs>";
+
+                cCoreScannerClass.ExecCommand(1001, ref inXML, out outXML, out status);
+
+                if (status != 0)
+                {
+                    Environment.Exit(2);
+                }
+
+                Thread.Sleep(-1);
+            }
+            catch (Exception x)
+            {
+                Console.Error.WriteLine(x.Message);
+            }
+        }
+
+        static void cCoreScannerClass_BarcodeEvent(short eventType, ref string pscanData)
+        {
+            try
+            {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(pscanData);
+
+                var xmlNode = xmlDoc.DocumentElement.SelectSingleNode("/outArgs/arg-xml/scandata/datalabel");
+
+                if (xmlNode == null)
+                {
+                    return;
+                }
+
+                var hexStrings = xmlNode.InnerText.Split(' ');
+                var hexBytes = new byte[hexStrings.Length];
+
+                for (var i = 0; i < hexStrings.Length; ++i)
+                {
+                    hexBytes[i] = Convert.ToByte(hexStrings[i], 16);
+                }
+
+                Console.WriteLine(Encoding.ASCII.GetString(hexBytes));
+            }
+            catch (Exception x)
+            {
+                Console.Error.WriteLine(x.Message);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# MotoBarScan
+
+A simple Windows Console application that prints to standard output barcode
+values read by the Motorola Barcode Scanners.
+
+## Requirements
+
+### .NET Framework
+
+  * __Version__: 4.x
+  * __Website__: http://www.microsoft.com/net
+  * __Download__: http://www.microsoft.com/net/downloads
+
+### Motorola CoreScanner SDK
+
+SDK is required for development. On client's machine, the CoreScanner Driver must be installed.
+
+  * __Version__: 2.x
+  * __Website__: [Scanner SDK for Windows](http://www.motorolasolutions.com/US-EN/Business+Product+and+Services/Software+and+Applications/Mobility+Software/Scanner+Drivers+and+Utilities/Scanner+SDK+for+Windows_US-EN)
+  * __Download__: [Software Development Kit (SDK) Download](https://portal.motorolasolutions.com/Support/US-EN/Resolution?solutionId=87666)
+
+## Usage
+
+Run the executable `MotoBarScan.exe` and try scanning a barcode!
+
+## License
+
+This project is released under the [MIT License](https://raw.github.com/morkai/MotoBarScan/master/license.md).
